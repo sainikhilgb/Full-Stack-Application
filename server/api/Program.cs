c55f36044c3564817f72c546889f12b7993a58fa
Nick <nick@Nicks-MacBook-Air.local>
2025-09-22 19:51:26 +0530

    gitignore

--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,6 +1,29 @@
+using efscaffold.Entities;
+using Infrastructure.Postgres.Scaffolding;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<MyDbContext>(conf=>conf.UseNpgsql(
+    builder.Configuration.GetSection("AppOptions:DbConnectionString").Value
+    ));
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
+{
+    var todoObject = new Todo()
+    {
+        Id = Guid.NewGuid().ToString(),
+        Title = "New Todo",
+        Description = "Test Todo",
+        Priority = 1,
+        Isdone = false
+    };
+    dbContext.Add(todoObject);
+    dbContext.SaveChanges();
+    var Todo = dbContext.Todos.ToList();
+    return Results.Ok(Todo);
+});
 
 app.Run();
