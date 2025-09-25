c0dfcde26a417f5704a904f6ff3f1cf96888b4ea
Nick <nick@Nicks-MacBook-Air.local>
2025-09-25 18:56:44 +0530

    deployment

---
+++ server/api/AppOptions.cs
@@ -0,0 +1,12 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace api;
+
+public class AppOptions
+{
+    [MinLength(1)]
+    public string DbConnectionString { get; set; }
+    [MinLength(1)]
+    public string JWTSecret { get; set; }
+}
--- server/api/Program.cs
+++ server/api/Program.cs
@@ -1,3 +1,4 @@
+using api;
 using efscaffold.Entities;
 using Infrastructure.Postgres.Scaffolding;
 using Microsoft.AspNetCore.Mvc;
@@ -5,12 +6,17 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<MyDbContext>(conf=>conf.UseNpgsql(
-    builder.Configuration.GetSection("AppOptions:DbConnectionString").Value
-    ));
+var appOptions = builder.Services.AddAppOptions(builder.Configuration);
+
+builder.Services.AddDbContext<MyDbContext>(conf => conf.UseNpgsql(appOptions.DbConnectionString));
+
+builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options =>
+{
+    options.SerializerOptions.TypeInfoResolver = new System.Text.Json.Serialization.Metadata.DefaultJsonTypeInfoResolver();
+});
 var app = builder.Build();
 
-app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
+app.MapGet("/", ([FromServices] MyDbContext dbContext) =>
 {
     var todoObject = new Todo()
     {
@@ -25,5 +31,7 @@ app.MapGet("/", ([FromServices]MyDbContext dbContext) =>
     var Todo = dbContext.Todos.ToList();
     return Results.Ok(Todo);
 });
+
+
 
 app.Run();
